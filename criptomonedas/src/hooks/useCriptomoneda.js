77e8433a451ef65dc06d41c0be85abd50f4df2a8
Héctor Daniel <hectorh.danielv@gmail.com>
2020-04-05 20:48:58 -0500

    criptomonedas

--- criptomonedas/src/hooks/useCriptomoneda.js
+++ criptomonedas/src/hooks/useCriptomoneda.js
@@ -21,12 +21,14 @@ const Select = styled.select`
     font-size: 1.2rem;
 `
 
-const useMoneda = (label, stateInicial, opciones) => {
+const useCriptomoneda = (label, stateInicial, opciones) => {
+
+    // console.log(opciones);
 
     // State de nuestro custom hook
     const [state, actualizarState] = useState(stateInicial);
 
-    const Seleccionar = () => (
+    const SelectCripto = () => (
         <Fragment>
             <Label>{label}</Label>
             <Select
@@ -35,14 +37,14 @@ const useMoneda = (label, stateInicial, opciones) => {
             >
                 <option value="">- Seleccione -</option>
                 {opciones.map(opcion => (
-                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
                 ))}
             </Select>
         </Fragment>
     );
 
     // Retornar state, interfaz y fn que modifica el state
-    return [state, Seleccionar, actualizarState];
+    return [state, SelectCripto, actualizarState];
 }
 
-export default useMoneda;
+export default useCriptomoneda;
